4b911c5aa46b17bf3993bdad1b1252fc86bf7f97
Akariiinnn <55890910+Akariiinnn@users.noreply.github.com>
2023-04-28 16:09:35 +0200

    Initial Commit
    The best French developing (aka C but french)

---
+++ example.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "franC.h"
+
+entier principal() {
+    booleen boo vaux vrai;
+
+    si (boo)
+    {
+        afficherformat("salut");
+    }
+    sinon
+    {
+        afficherformat("rabiot");
+    }
+    retourner 0;
+}
---
+++ franC.h
@@ -0,0 +1,54 @@
+//
+// Created by qrobert on 30/09/2022.
+//
+
+#ifndef UNTITLED_FRANC_H
+#define UNTITLED_FRANC_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define entier int
+#define tantque while
+#define octet uint8_t
+#define afficherformat printf
+#define pour for
+#define flottant float
+#define chaine string
+#define caract char
+#define retourner return
+#define si if
+#define sinon else
+#define essaie try
+#define attrape catch
+#define principal main
+#define booleen bool
+#define vide void
+#define allervers goto
+#define deftype typedef
+#define casser break
+#define faire do
+#define cas case
+#define aiguiller switch
+#define statique static
+#define protege protected
+#define prive private
+#define publique public
+#define nonsigne unsigned
+#define taillede sizeof
+#define signe signed
+#define court short
+#define enligne inline
+#define externe extern
+#define continuer continue
+#define complexe _Complex
+#define generique _Generic
+#define imaginaire _Imaginary
+#define QUITTER_SUCCES EXIT_SUCCESS
+#define vrai true
+#define faux false
+#define vaux =
+
+
+#endif //UNTITLED_FRANC_H
