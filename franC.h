0050110aaad9b0ef9c87d0725a3e095126dd680c
Akariiinnn <55890910+Akariiinnn@users.noreply.github.com>
2023-04-28 17:09:43 +0200

    stdio.h translation
    we cannot be stopped (fromage)

--- franC.h
+++ franC.h
@@ -10,14 +10,15 @@
 #include <stdbool.h>
 
 #define entier int
+#define assembleur asm
 #define tantque while
 #define octet uint8_t
-#define afficherformat printf
 #define pour for
 #define flottant float
 #define chaine string
 #define caract char
 #define retourner return
+#define automatique auto
 #define si if
 #define sinon else
 #define essaie try
@@ -25,6 +26,7 @@
 #define principal main
 #define booleen bool
 #define vide void
+#define oiseau volatile
 #define allervers goto
 #define deftype typedef
 #define casser break
@@ -45,10 +47,92 @@
 #define complexe _Complex
 #define generique _Generic
 #define imaginaire _Imaginary
-#define QUITTER_SUCCES EXIT_SUCCESS
 #define vrai true
 #define faux false
 #define vaux =
+#define longueurde strlen
+#define nul NULL
+#define nouveau new
+#define supprimer delete
+#define zero memset
+#define et &&
+#define ou ||
+#define non !
+#define etlogique &
+#define oulogique |
+#define ouexclusif ^
+#define minimum min
+#define maximum max
+#define constante const
+#define entiersigne signed int
+#define flottantsigne signed float
+#define doubleprecision double
+#define longdouble long double
+#define entiercourt short int
+#define entiernonsigne unsigned int
+#define octetnonsigne unsigned char
+#define allouemem malloc
 
+// Stdio
+
+#define taille_t size_t
+#define FICHIER FILE
+#define entree scanf
+#define afficherformat printf
+#define posf_t fpos_t
+#define FDF EOF
+#define fouvre fopen
+#define fouvre64 fopen64
+#define fouvre_s fopen_s
+#define fafficherformat fprintf
+#define fafficherformat_s fprintf_s
+#define fferme fclose
+#define ferreur ferror
+#define favoirposition fgetpos
+#define favoirc fgetc
+#define favoircaractere fgetchar
+#define flire fread
+#define flire_s fread_s
+#define fchercher fseek
+#define fcherchero fseeko
+#define fchercher64 fseek64
+#define fmettreposition fsetpos
+#define fmettreposition64 fsetpos64
+#define fraconte ftell
+#define fraconteo ftello
+#define fraconteo64 ftello64
+#define fecrire fwrite
+#define ffermetout fcloseall
+#define ffdf feof
+#define renommer rename
+#define retirer remove
+#define revenirenarrire rewind
+#define mettretampon setbuf
+#define fichiertemp tmpfile
+#define nomtemp tmpfile
+#define safficherformat sprintf
+#define safficherformat_s sprintf_s
+#define vfafficherformat vfprintf
+#define vfafficherformat_s vfprintf_s
+#define vafficherformat vprintf
+#define fentree fscanf
+#define sentree ssscanf
+#define favoirchaine fgets
+#define fmettrecaractere fputc
+#define fmettrechaine fputs
+#define avoirc getc
+#define avoirchaine gets
+#define mettrec putc
+#define mettrecaract putchar
+#define mettrechaine puts
+#define perdrec ungetc
+#define perreur
+#define avoircaract
+#define TAILTAMP BUFSIZ
+#define MAX_NOMFICHIER FILENAME_MAX
+#define MAX_FOUVRE FOPEN_MAX
+
+// Stdlib
+#define QUITTER_SUCCES EXIT_SUCCESS
 
 #endif //UNTITLED_FRANC_H
